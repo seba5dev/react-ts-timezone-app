310bdf7367123a88745dde3e647c0896f2fc2d2b
Sebastian Martínez <35276965+smartinez05@users.noreply.github.com>
2022-06-20 15:11:48 -0500

    limpiar css no usado y mejorar apariencia del sitio

--- src/components/Clock/styles.tsx
+++ src/components/Clock/styles.tsx
@@ -5,14 +5,9 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  text-align: center;
   justify-content: center;
   min-height: 100vh;
   width: 100%;
-
-  @media ${device.mobileS} {
-    max-width: 100%;
-  }
 `;
 
 export const Title = styled.h1`
@@ -28,4 +23,35 @@ export const Text = styled.p`
   font-size: 1.5rem;
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica,
     Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+`;
+
+export const BigText = styled(Text)`
+  font-size: 2rem;
+  font-weight: bold;
+  color: #2e2e2e;
+  margin-bottom: 1rem;
+`;
+
+export const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  width: auto;
+  background-color: #fff2f2;
+  border-radius: 1rem;
+  box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.4);
+  padding: 1rem;
+  margin: 1rem 0;
+
+  @media ${device.laptop} {
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-evenly;
+    width: 80%;
+    padding: 3em;
+    margin: 1em;
+  }
 `;
